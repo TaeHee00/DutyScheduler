6c5c761cb12481f39f27d1cc63f55d0a3c8aed7a
Tae Hee Jang <kancth03@gmail.com>
2025-04-20 23:31:57 +0900

    feat: main.tsx Route 설정

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,23 @@ import App from './App.tsx'
 import {BrowserRouter, Routes, Route} from "react-router";
 import LoginPage from "./pages/LoginPage.tsx";
 import SchedulerPage from "./pages/SchedulerPage.tsx";
+import GlobalStyle from "./styles/global.ts";
+import StatisticsPage from "./pages/StatisticsPage.tsx";
+import MembersPage from "./pages/MembersPage.tsx";
+import ChangePage from "./pages/ChangePage.tsx";
 
 createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
-    <Routes>
-      <Route path={"/"} element={<App />} />
-      <Route path={"/login"} element={<LoginPage />} />
-      <Route path={"/schedule"} element={<SchedulerPage />} />
-    </Routes>
-  </BrowserRouter>
+  <>
+    <GlobalStyle />
+    <BrowserRouter>
+      <Routes>
+        <Route path={"/"} element={<App />} />
+        <Route path={"/login"} element={<LoginPage />} />
+        <Route path={"/schedule"} element={<SchedulerPage />} />
+        <Route path={"/statistics"} element={<StatisticsPage />} />
+        <Route path={"/members"} element={<MembersPage />} />
+        <Route path={"/changes"} element={<ChangePage />} />
+      </Routes>
+    </BrowserRouter>
+  </>
 )
